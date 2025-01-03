8a78815801ab375e89ffc02e64a8eafb8bc1eda9
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-03 16:40:39 -0600

    change LDFLAGS back

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-set PKG_LIBS="-L%LIBRARY_LIB:\=/% -ludunits2 -lexpat -Wl,--allow-shlib-undefined"
+set PKG_LIBS="-L%LIBRARY_LIB:\=/% -ludunits2 -lexpat"
 set PKG_CPPFLAGS="-I%LIBRARY_INC:\=/%"
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
