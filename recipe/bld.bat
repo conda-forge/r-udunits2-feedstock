af4fd58248eeee0892a6588d7897794a641e7e24
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-03 14:33:11 -0600

    try new strategy

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-set PKG_LIBS="-Wl,--no-as-needed -L%LIBRARY_LIB:\=/% -ludunits2 -lexpat"
+set PKG_LIBS="-L%LIBRARY_LIB:\=/% -ludunits2 -lexpat -Wl,--no-as-needed"
 set PKG_CPPFLAGS="-I%LIBRARY_INC:\=/%"
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
