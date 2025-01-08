79e7dde5a34f5c3bc8581632e98d6c019f97e06c
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-07 21:05:49 -0800

    don't set -L flag

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
-set PKG_LIBS="-L%LIBRARY_LIB:\=/% -ludunits2 -lexpat"
-set PKG_CPPFLAGS="-I%LIBRARY_INC:\=/%"
+REM set PKG_LIBS="-L%LIBRARY_LIB:\=/% -ludunits2 -lexpat"
+REM set PKG_CPPFLAGS="-I%LIBRARY_INC:\=/%"
+set PKG_LIBS="-ludunits2 -lexpat"
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
