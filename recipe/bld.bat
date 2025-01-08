34fd2fcf96f44b6fbc57b10fc1eb2181c675498c
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-08 09:27:52 -0800

    add back expat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 REM set PKG_LIBS="-L%LIBRARY_LIB:\=/% -ludunits2 -lexpat"
 REM set PKG_CPPFLAGS="-I%LIBRARY_INC:\=/%"
-set PKG_LIBS=-ludunits2
+set PKG_LIBS=-ludunits2 -lexpat
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
